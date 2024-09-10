48808c409ccef9b725efa231698019282bf55c44
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-09-10 22:05:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './assets/Header'
+
+const  App=()=> {
+  
+
+      return (
+       
+        <Header></Header>
+
+        
+      );
+    }
+
+export default App
---
+++ src/assets/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+ 
+      <h1 className="    text-center  text-3xl font-bold underline bg-cyan-700">TODO List</h1>
+    
+  )
+}
+
+export default Header;
