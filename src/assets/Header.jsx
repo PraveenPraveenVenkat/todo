ee64c530eff25e9b9078b29386db62a766fd68bb
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-09-14 20:55:46 +0530

    Api Fetch I Front-end

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import {useState} from 'react'
+
 
 const Header = () => {
+
+  const [title,setTitle] =useState ("");
+  const [Description setDesscription] =useState ("");
+  const [todos,setTods] =useState ([])
+  const apiurl = "http://localhost:3000/"
+
+
+  const  handleSubmit = () => {
+    if (title.trim () !== ''  && Description.trim()!==  '')
+      fetch (apiurl)
+  }
   return (
     <>
     <div className="text-white text-xl p-6 bg-cyan-700">
@@ -12,24 +24,26 @@ const Header = () => {
             <p>Insert The Items</p>
 
 </div>
-<div className='grid gap-4 grid-cols-2'>
+<div className='grid gap-4 grid-cols-3'>
            <input type="text" id="name" name="name" placeholder="Title "/> 
 
-           <input type="Description" id="name" name="name" placeholder="Title "/> 
+           <input type="Description" id="name" name="name" placeholder="Description " /> 
 
-            <button className='bg-black text-white text-right'>Submmit</button>
+            <button className='bg-lime-600 text-white text-right'>Submmit</button>
             </div>
 
 
-<div className='grid gap-4 grid-cols-2'>
-  <h2>Tasks </h2>
-  <p>Insert The Items</p>
+
+   <div className='grid gap-4 grid-cols-3'>
   <button className='bg-red-500 text-white text-right'>Submmit</button>
             
   <button className='bg-lime-600 text-white text-right'>Submmit</button>
+
+  <input type="Description" id="name" name="name" placeholder="Description " /> 
             
-      
-  </div>
+       
+      </div> 
+  
           
             </>
 )
