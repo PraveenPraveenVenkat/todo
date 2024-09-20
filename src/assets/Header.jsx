09a9334951a771ec3cefb51b912add9045390d97
PraveenPraveenVenkat <praveenvenkatachalam2001@gmail.com>
2024-09-20 21:52:18 +0530

    Task Spam Completed

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -4,14 +4,14 @@ import {useState} from 'react'
 const Header = () => {
 
   const [title,setTitle] =useState ("");
-  const [Description setDesscription] =useState ("");
-  const [todos,setTods] =useState ([])
+  const [Description ,setDescription] = useState ("");
+   const [todos,setTods] =useState ([])
   const apiurl = "http://localhost:3000/"
 
 
   const  handleSubmit = () => {
     if (title.trim () !== ''  && Description.trim()!==  '')
-      fetch (apiurl)
+      fetch (apiurl= "http://localhost:3000/")
   }
   return (
     <>
@@ -20,31 +20,34 @@ const Header = () => {
           </div>
 
           <div>
-            <h3>Add  Items</h3>
-            <p>Insert The Items</p>
+            <h3 className='font-bold text-2xl mt-5  text-left'>Add  Items</h3>
+           
 
 </div>
-<div className='grid gap-4 grid-cols-3'>
-           <input type="text" id="name" name="name" placeholder="Title "/> 
+<div className='flex items-start  grid gap-4 grid-cols-3  mt-7 '>
+           <input type="text"className='border-2 rounded-lg' onChange={(e)=> setTitle (e.target.value)} value={title} placeholder="Title "/> 
 
-           <input type="Description" id="name" name="name" placeholder="Description " /> 
+           <input type="Description" className='border-2 rounded-lg' onChange={(e)=> setDescription (e.target.value)} value={Description} placeholder="Description " /> 
 
-            <button className='bg-lime-600 text-white text-right'>Submmit</button>
+            <button className='bg-black text-white  text-center ml-24 rounded-full'>Submmit</button>
             </div>
 
-
-
-   <div className='grid gap-4 grid-cols-3'>
-  <button className='bg-red-500 text-white text-right'>Submmit</button>
-            
-  <button className='bg-lime-600 text-white text-right'>Submmit</button>
-
-  <input type="Description" id="name" name="name" placeholder="Description " /> 
-            
-       
-      </div> 
-  
+            <h3 className='font-bold  text-2xl mt-5  text-left'>Tasks</h3>
+            <ul>
+              <li className='flex items-start border-red-400 grid gap-36 grid-cols-3 mt-4  '>
+                 <span className='gap-x-8   gap-36 '>Item  Text</span>
+                 <div>
+                 <button className='bg-amber-400 rounded-lg ' >Edit</button>
+                 <button className='bg-red-600 rounded-lg '>Delete</button>
+                 </div>
+               
+              </li>
+            </ul>
           
+          <div>
+            
+
+</div>
             </>
 )
 }
